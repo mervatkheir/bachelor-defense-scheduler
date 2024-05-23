65dd64fca3cd7a07c1b009383e2e21be35c3c80c
Ahmed Khedr <ahmedkhedr2910@gmail.com>
2024-05-23 17:35:47 +0300

    setup tanstack query

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,20 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Home from "./screens/Home/Home";
 import ToastNotification from "./components/Toast/ToastNotification";
+import { useQuery } from "@tanstack/react-query";
 
 function App() {
+  const { data, isLoading } = useQuery({
+    queryKey: ["todo"],
+    queryFn: () =>
+      fetch("https://jsonplaceholder.typicode.com/todos").then((res) =>
+        res.json()
+      ),
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
   return (
     <Router>
       <ToastNotification />
